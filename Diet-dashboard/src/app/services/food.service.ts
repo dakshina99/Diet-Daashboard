47b1089bcdaa4e655d38bd1a0b0334473a8cab9f
KaveeshaPiumini <piumini95kaveesha@gmail.com>
2022-04-08 15:14:35 +0530

    first

---
+++ Diet-dashboard/src/app/services/food.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+// import { Observable } from "rxjs/Observable";
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodService {
+
+  constructor() { }
+}
